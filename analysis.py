24b110b35f72a850eb046b6ad2c66731520d403d
ofacklam <oliver.facklam@gmail.com>
2020-03-25 23:29:31 +0100

    :lipstick: add gifs

--- analysis.py
+++ analysis.py
@@ -19,8 +19,8 @@ def get_data(file):
 # In[33]:
 
 
-labels = ['A2C / no pen.', 'A2C / death pen.', 'DQN / no pen.', 'DQN / death pen.', 'DQN w/ pen. (save)']
-data_folders = ['task3/a2c_nopen_test.txt', 'task3/a2c_small_deathpen_test.txt', 'task3/dqn_nopen_test.txt', 'task3/dqn_small_deathpen_test.txt', 'task3/dqn_save_deathpen_test.txt']
+labels = ['A2C / no pen.', 'A2C / death pen.', 'DQN / no pen.', 'DQN / death pen.']
+data_folders = ['task3/a2c_nopen_test.txt', 'task3/a2c_small_deathpen_test.txt', 'task3/dqn_nopen_test.txt', 'task3/dqn_save_deathpen_test.txt']
 
 
 # In[34]:
@@ -39,6 +39,13 @@ plt.show()
 # In[23]:
 
 
-plt.plot(get_data('task3/dqn_small_deathpen_test.txt'), 'o')
-#plt.plot(get_data('task3/dqn_nopen_train.txt'), 'o')
+plt.plot(get_data('task3/a2c_nopen_train.txt'), 'o', label='a2c agent')
+plt.plot(get_data('task3/dqn_nopen_train.txt'), 'o', label='dqn agent')
+plt.xlabel('Training epoch')
+plt.ylabel('Distance run')
+plt.legend()
+plt.show()
+
+
+
 
